a67ad034bf546d2949728ba09c9935b7b50d9377
Prasanth <141898438+prasanthj2023@users.noreply.github.com>
2024-01-08 14:16:48 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Carousel from "./components/Carousel";
 import "./App.css";
 
-
-
 export default class App extends Component {
   render() {
     return (
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,12 +2,53 @@ import React, { Component } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
 // you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      index: 2,
+    };
+  }
+
+  render() {
+    let prevImage = () => {
+      this.setState((prevState) => ({
+        index: (prevState.index - 1 + images.length) % images.length,
+      }));
+    };
+
+    let nextImage = () => {
+      this.setState((prev) => ({
+        index: (prev.index + 1) % images.length,
+      }));
+    };
+
+    return (
+      <div id="main">
+        <div className="btn">
+          <ArrowBackIosIcon onClick={prevImage} />
+        </div>
+        <div>
+          <div id="image">
+            <img src={images[this.state.index].img} />
+            <h1 className="abs-title">
+              <span>{images[this.state.index].title}</span>
+            </h1>
+            <p className="abs-sub">
+              <span>{images[this.state.index].subtitle}</span>
+            </p>
+          </div>
+        </div>
+        <div className="btn">
+          <ArrowForwardIosIcon onClick={nextImage} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
